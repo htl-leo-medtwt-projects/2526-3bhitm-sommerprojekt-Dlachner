1b81b750c0a635241c8e8dd47e296a8ea72993ad
Dlachner <d.lachner@students.htl-leonding.ac.at>
2026-04-07 14:57:34 +0200

    Navbar erstellt

--- project/docker_php/htdocs/projekt/mainpage.php
+++ project/docker_php/htdocs/projekt/mainpage.php
@@ -8,6 +8,33 @@
 </head>
 <body>
     <!-- Navbar -->
-     
+    <div class="navbar">
+        <div class="left">
+            <div class="logo">
+            <img src="images/logo.png" alt="Logo">
+            </div>
+
+            <div class="nav-links">
+            <a href="#">Zum Shop</a>
+            <a href="#">Zum Konfigurator</a>
+            </div>
+        </div>
+
+        <div class="right">
+            <div class="icon">
+            <img src="images/wishlist.png" alt="Herz">
+            <span>Merkliste</span>
+            </div>
+
+            <div class="icon">
+            <img src="images/warenkorb.png" alt="Warenkorb">
+            <span>Warenkorb</span>
+            </div>
+
+            <div class="profile">
+            <img src="images/profilpic.png" alt="Profil">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
