c779b58f81178f310f9cb8388a799dc9ed76736c
Dlachner <d.lachner@students.htl-leonding.ac.at>
2026-04-07 15:19:35 +0200

    Mainpage erstellt

--- project/docker_php/htdocs/projekt/mainpage.php
+++ project/docker_php/htdocs/projekt/mainpage.php
@@ -15,26 +15,69 @@
             </div>
 
             <div class="nav-links">
-            <a href="#">Zum Shop</a>
-            <a href="#">Zum Konfigurator</a>
+                <a href="#">Zum Shop</a>
+                <a href="#">Zum Konfigurator</a>
             </div>
         </div>
 
         <div class="right">
             <div class="icon">
-            <img src="images/wishlist.png" alt="Herz">
-            <span>Merkliste</span>
+                <img src="images/wishlist.png" alt="Herz">
+                <span>Wunschliste</span>
             </div>
 
             <div class="icon">
-            <img src="images/warenkorb.png" alt="Warenkorb">
-            <span>Warenkorb</span>
+                <img src="images/warenkorb.png" alt="Warenkorb">
+                <span>Warenkorb</span>
             </div>
 
             <div class="profile">
-            <img src="images/profilpic.png" alt="Profil">
+                <img src="images/profilpic.png" alt="Profil">
             </div>
         </div>
     </div>
+    <!-- -->
+    <!-- HERO SECTION -->
+    <section class="hero">
+        <div class="hero-content">
+            <h1>Konfiguriere dein<br>Skateboard!</h1>
+            <button>Zum Konfigurator</button>
+        </div>
+    </section>
+
+    <!-- KATEGORIEN -->
+    <section class="categories">
+        <h2>Kategorien</h2>
+
+        <div class="category-grid">
+
+            <div class="card large">
+                <h3>Decks</h3>
+                <img src="images/categoryParts/deck.png" alt="Deck">
+            </div>
+
+            <div class="card">
+                <h3>Rollen</h3>
+                <img src="images/categoryParts/rollen.png" alt="Rollen">
+            </div>
+
+            <div class="card">
+                <h3>Achsen</h3>
+                <img src="images/categoryParts/achsen.png" alt="Achsen">
+            </div>
+
+            <div class="card">
+                <h3>Kugellager</h3>
+                <img src="images/categoryParts/kugellager.png" alt="Lager">
+            </div>
+
+            <div class="card">
+                <h3>Zubehör</h3>
+                <img src="images/categoryParts/tools.png" alt="Zubehör">
+            </div>
+
+        </div>
+    </section>
+
 </body>
 </html>
