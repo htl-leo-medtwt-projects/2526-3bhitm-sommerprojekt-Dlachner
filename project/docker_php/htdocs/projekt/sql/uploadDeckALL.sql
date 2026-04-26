876ec74ee6fb68af3119d92ab400e94001e8de83
Dlachner <d.lachner@students.htl-leonding.ac.at>
2026-04-26 09:59:53 +0200

    Neue Decks hinzugefügt

--- project/docker_php/htdocs/projekt/sql/uploadDeckALL.sql
+++ project/docker_php/htdocs/projekt/sql/uploadDeckALL.sql
@@ -53,3 +53,108 @@ INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
 
 INSERT INTO deck (product_id, width, length) VALUES
 (LAST_INSERT_ID(), 8.38, 32);
+
+-- Produkt 10
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Single Skull 8.0″ Skateboard Deck', 74.95, 'Zero', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8, 31.6);
+
+-- Produkt 11
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Misfits Fiend Skull Gitd 8.5″ Skateboard Deck',84.95, 'Zero', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.5, 32.25);
+
+-- Produkt 12
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Cold Pressed Boo 8.25″ Skateboard Deck',84.95, 'DGK', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.25, 31.85);
+
+-- Produkt 13
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Gernika 8.25″x31.85″ LC Skateboard Deck',59.95, 'Jart', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.25, 31.85);
+
+-- Produkt 14
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Blood Skull 8.25″ Skateboard Deck',79.95, 'Zero', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.5, 32.3);
+
+-- Produkt 15
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Stay High 8.0″x31.44″ HC Skateboard Deck',59.95, 'Jart', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8, 31.44);
+
+-- Produkt 16
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Blood Skull 8.5″ Skateboard Deck',79.95, 'Zero', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.5, 32.3);
+
+-- Produkt 17
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Classic Dot 8.375″ Skateboard Deck',79.95, 'Santa_Cruz', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.375, 31.83);
+
+-- Produkt 18
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Misfits Zero Business Gitd 8.25″ Skateboard Deck',84.95, 'Zero', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.25, 31.9);
+
+-- Produkt 19
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Malto 93 Til Khaki 8.5″ Skateboard Deck',79.95, 'Girl', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.5, 31.875);
+
+-- Produkt 20
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Single Skull 8.5″ Skateboard Deck',74.95, 'Zero', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.5, 32.3);
+
+-- Produkt 21
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Bold Black 8.5″ Skateboard Deck',74.95, 'Zero', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.5, 32.3);
+
+-- Produkt 22
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Bold Black 8.25″ Skateboard Deck',74.95, 'Zero', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.25, 31.9);
+
+-- Produkt 23
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Burleigh Fright Night 8.5″ Skateboard Deck',79.95, 'Zero', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.5, 32.25);
+
+-- Produkt 24
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('X Pleasures Reaper 8.25″ Skateboard Deck',84.95, 'Zero', 'deck');
+
+INSERT INTO deck (product_id, width, length) VALUES
+(LAST_INSERT_ID(), 8.25, 31.9);
