25b0fb2500c87b5507658c9c14a1c3b1cefc023a
Dlachner <d.lachner@students.htl-leonding.ac.at>
2026-04-27 13:28:30 +0200

    neue wheels zum upload vorbereitet

---
+++ project/docker_php/htdocs/projekt/sql/upload.sql
@@ -0,0 +1,126 @@
+-- Produkt 36       Bild: oj_wheels1_1, oj_wheels1_2, oj_wheels1_3
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Black Sabbath Paranoid Chubbies 99a 56mm Rollen',51.95, 'OJ_Wheels', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 24, 56, '99A');
+
+-- Produkt 37       Bild: welcome3_1, welcome3_2, welcome3_3, welcome3_4
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Orbs Apparitions 52mm Rollen',34.95, 'Welcome', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 20, 52, '99A');
+
+-- Produkt 38       Bild: welcome4_1, welcome4_2, welcome4_3, welcome4_4
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Orbs Apparitions 56mm Rollen',34.95, 'Welcome', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 22, 56, '99A');
+
+-- Produkt 39       Bild: spitfire2_1, spitfire2_2
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('X Thrasher TrashBurn 80HD CF 56mm Rollen',71.95, 'Spitfire', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 25, 56, '88HD');
+
+-- Produkt 40       Bild: spitfire3_1, spitfire3_2, spitfire3_3
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Orbs Specters 53mm Rollen',34.95, 'Spitfire', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 22, 53, '99A');
+
+-- Produkt 41       Bild: spitfire4_1, spitfire4_2, spitfire4_3
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Orbs Apparitions 52mm Rollen',34.95, 'Spitfire', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 20, 52, '99A');
+
+-- Produkt 42       Bild: spitfire5_1, spitfire5_2
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('X Thrasher Oath F499 Radial Full 58mm Rollen',74.95, 'Spitfire', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 25, 58, '99A');
+
+-- Produkt 43       Bild: jart1_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Uproar 54mmx34mm 84a Rollen',46.95, 'Jart', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 20, 54, '84A');
+
+-- Produkt 44       Bild: jart2_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Gernika 53mm 99A Classic Shape Rollen',34.95, 'Jart', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 17, 53, '99A');
+
+-- Produkt 45       Bild: jart3_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Uproar 56mmx34mm 84a Rollen',46.95, 'Jart', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 20, 56, '84A');
+
+-- Produkt 46       Bild: cruzade1_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Squirt 58mm 84A Black Rollen',34.95, 'Cruzade', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 22, 58, '84A');
+
+-- Produkt 47       Bild: jart4_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Skeletons 54mm 99A Rollen',46.95, 'Jart', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 20, 54, '99A');
+
+-- Produkt 48       Bild: jart5_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Gernika 51mm 99A Classic Shape Rollen',34.95, 'Jart', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 16, 51, '99A');
+
+-- Produkt 49       Bild: jart6_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Foo Fighters 51mm 99A Rollen',46.95, 'Jart', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 18, 51, '99A');
+
+-- Produkt 50       Bild: jart7_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Skeletons 52mm 99A Rollen',46.95, 'Jart', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 18, 52, '99A');
+
+-- Produkt 51       Bild: jart8_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Skeletons 55mm 99A Rollen',46.95, 'Jart', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 18, 55, '99A');
+
+-- Produkt 52       Bild: jart9_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Foo Fighters 53mm 99A Rollen',46.95, 'Jart', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 18, 53, '99A');
+
+-- Produkt 53       Bild: cruzade2_1
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Squirt 56mm 84A Black Rollen',34.95, 'Cruzade', 'wheels');
+
+INSERT INTO wheels (product_id, width, durchmesser, typ) VALUES
+(LAST_INSERT_ID(), 20, 56, '84A');
+
