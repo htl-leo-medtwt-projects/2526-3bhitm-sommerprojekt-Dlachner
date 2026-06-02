f29525383757459bfa5787187bd4641fdab0ff10
Dlachner <d.lachner@students.htl-leonding.ac.at>
2026-06-02 09:27:58 +0200

    Bearings eingefügt

---
+++ project/docker_php/htdocs/projekt/sql/uploadBearingsALL.sql
@@ -0,0 +1,83 @@
+-- Produkt 61
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Reds Kugellager',24.95, 'Bones_Bearings', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Bearings Steel');
+
+-- Produkt 62
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Super Reds Kugellager',39.95, 'Bones_Bearings', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Bearings Steel');
+
+-- Produkt 63
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Super Swiss 6 Balls Kugellager',94.95, 'Bones_Bearings', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Bearings Steel');
+
+-- Produkt 64
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Reds Big Balls Kugellager',31.95, 'Bones_Bearings', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Bearings Steel');
+
+-- Produkt 65
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Classics Kugellager',19.95, 'Spitfire', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Bearings Steel');
+
+-- Produkt 66
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Burners Kugellager',34.95, 'Spitfire', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Bearings Steel');
+
+-- Produkt 67
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Ceramic Sarmiento Kugellager',71.95, 'Mosaic', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Ceramic');
+
+-- Produkt 68
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Ceramic Surrey Kugellager',71.95, 'Mosaic', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Ceramic');
+
+-- Produkt 69
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Ceramic Kremer Kugellager',71.95, 'Mosaic', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Ceramic');
+
+-- Produkt 70
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Ceramic Penny Kugellager',71.95, 'Mosaic', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Ceramic');
+
+-- Produkt 71
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('Ceranium Kugellager',77.95, 'Mosaic', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Ceranium');
+
+-- Produkt 72
+INSERT INTO product (beschreibung, preis, brand, kategorie) VALUES
+('G2 Kugellager',24.95, 'Bronson', 'bearings');
+
+INSERT INTO bearings (product_id, material) VALUES
+(LAST_INSERT_ID(), 'Bearings Steel');
